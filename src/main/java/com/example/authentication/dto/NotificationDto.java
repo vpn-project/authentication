6f9a4fd5894ad28e3f63fb9b26dd301f8ceaac6e
Kirill <kirill.vorobyev2002@yandex.ru>
2024-01-24 01:16:27 +0300

    feat(kafka): add dto for kafka

---
+++ src/main/java/com/example/authentication/dto/NotificationDto.java
@@ -0,0 +1,12 @@
+package com.example.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationDto {
+    private String description;
+}
